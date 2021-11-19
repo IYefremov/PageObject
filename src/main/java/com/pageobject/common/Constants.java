ab9ebfee54930fc2d79b5030ee6c1f7b7aa57a10
IvanYefremov <iyefremov@yahoo.com>
2021-11-19 10:52:30 +0200

    update

--- src/main/java/com/pageobject/common/Constants.java
+++ src/main/java/com/pageobject/common/Constants.java
@@ -3,5 +3,4 @@ package com.pageobject.common;
 public class Constants {
     public static final String CHROME_DRIVER_PATH = "G:\\Cyberiansoft\\Automation\\JavaProjects\\Tools" +
             "\\Drivers\\ChromeDriver96\\chromedriver.exe";
-
 }
